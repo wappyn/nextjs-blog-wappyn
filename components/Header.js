11e76ba732b350febe63165112b21a6bd1f2bf94
Wappyn <wappyn@gmail.com>
2022-05-31 20:18:38 +0200

    zmiana z tym banerem

--- components/Header.js
+++ components/Header.js
@@ -4,8 +4,7 @@ import Image from 'next/image'
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="banner" ><Image src="/banner.jpg" alt="wappyn" /></div>
-      // <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
+      <div className="banner" ><Image src="/banner.jpg" alt="wappyn" width="256" height="115"/></div>
       <p className="text-2xl dark:text-white text-center">
         <Link href="/">
           <a>{name}</a>
